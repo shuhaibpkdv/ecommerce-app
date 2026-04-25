ef25ae1d155e24b1a82eddd0c8e0c0be4b606ccf
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2026-04-25 21:05:39 +0530

    item in cart count

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./App.css"
 import products from "./data/products";
 import ProductCard from "./components/ProductCard";
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart([...cart, product])
+  }
+
   return (
     <div className="container">
       <h1>My Store</h1>
 
+      <h2>Cart: {cart.length} items</h2>
+
       <div className="grid">
         {products.map((p) => (
-          <ProductCard key={p.id} product={p} />
+          <ProductCard key={p.id} product={p} addToCart={addToCart} />
         ))}
       </div>
     </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./ProductCard.css"
 
-function ProductCard({ product }) {
+function ProductCard({ product, addToCart }) {
   return (
     <div className='card'>
-      <img src={product.image} width="100%" />
+      <img src={product.image} />
+
       <h3>{product.name}</h3>
       <h3>{product.price}</h3>
-      <button>Add to Cart</button>
+
+      <button onClick={() => addToCart(product)}>
+        Add to Cart
+      </button>
     </div>
   )
 }
