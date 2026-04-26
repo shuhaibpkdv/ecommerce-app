7fd1c398418ca33cb55d14d5e86000d45526b894
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2026-04-26 14:42:27 +0530

    same product quantity increases

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ function App() {
   const [cart, setCart] = useState([])
 
   const addToCart = (product) => {
-    setCart([...cart, product])
+    const exist = cart.find((item) => item.id === product.id)
+
+    if(exist) {
+      const updatedCart = cart.map((item) => 
+        item.id === product.id
+          ? { ...item, qty: item.qty + 1}
+          : item
+      )
+      setCart(updatedCart)
+    } else {
+      setCart([...cart, {...product, qty: 1}])
+    }
   }
 
-  const total = cart.reduce((sum, item) => sum + item.price, 0)
+  const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0)
+
+  const removeFromCart = (indexToRemove) => {
+    const updatedCart = cart.filter((_, index) => index !== indexToRemove)
+    setCart(updatedCart)
+  }
 
   return (
     <div className="container">
@@ -23,7 +39,11 @@ function App() {
       <div>
         {cart.map((item, index) => (
           <div key={index}>
-            {item.name} - ${item.price}
+            {item.name} x {item.qty} - ₹{item.price * item.qty}
+
+            <button onClick={() => removeFromCart(index)}>
+              Remove
+            </button>
           </div>
         ))}
       </div>
