0906f7e0553e988b6878edaee897704cd118849f
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2026-04-26 16:31:51 +0530

    changed cart page design

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
+import "./CartPage.css"
 
 function CartPage({ cart, increaseQty, decreaseQty, removeFromCart}) { 
   const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0)
 
   return (
-    <div>
-      <h2>Your Cart</h2>
+    <div className='cart-container'>
+      <h2 className='cart-title'>Your Cart</h2>
 
       {cart.length === 0? (
         <p>Cart is empty</p>
       ): (
         <>
           {cart.map((item, index) => (
-            <div key={item.id}>
-              {item.name} - ₹{item.price * item.qty}
+            <div className='cart-item' key={item.id}>
+              <div className='cart-info'>
+                <h4>{item.name}</h4>
+                <p>₹{item.price * item.qty}</p>
+              </div>
 
-              <button onClick={() => decreaseQty(item.id)}> - </button>
-              <span> {item.qty} </span>
-              <button onClick={() => increaseQty(item.id)}> + </button>
+              <div className='cart-actions'>
+                <button onClick={() => decreaseQty(item.id)}> - </button>
+                <span> {item.qty} </span>
+                <button onClick={() => increaseQty(item.id)}> + </button>
 
-              <button onClick={() => removeFromCart(index)}>
-                Remove
-              </button>
+                <button onClick={() => removeFromCart(index)}>
+                  Remove
+                </button>
+              </div>
             </div>
           ))}
 
-          <h3>Total: ₹{total}</h3>
+          <div className='total'>Total: ₹{total}</div>
         </>
       )}
     </div>
