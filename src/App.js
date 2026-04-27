61a2b415c91a5b94d6094860ae9e31b05ea1b6fc
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2026-04-27 12:38:01 +0530

    resolve build error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css"
 import Navbar from "./components/Navbar"
